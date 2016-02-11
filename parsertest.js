a66bf4c92863450f05c2b46b6b5cca77b35106b2
John <wildfireone@gmail.com>
2016-02-11 18:25:48 +0000

    commandline

--- parsertest.js
+++ parsertest.js
@@ -3,12 +3,15 @@
  */
 
 var inputfile = process.argv.slice(2)[0];
-console.log(inputfile);
-fs = require('fs')
-fs.readFile(inputfile, 'utf8', function (err,data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
+console.log("Input file: " +inputfile);
+
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream(inputfile)
+});
+
+lineReader.on('line', function (line) {
+
+    console.log('Line from file:', line);
+
 });
 
