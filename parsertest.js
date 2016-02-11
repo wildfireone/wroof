cc9c0d9dc668e1e21f82525591c07ed7dfe867b9
John <wildfireone@gmail.com>
2016-02-11 18:19:15 +0000

    commandline

--- parsertest.js
+++ parsertest.js
@@ -2,5 +2,12 @@
  * Created by Isaacs on 11/02/2016.
  */
 
-console.log(process.argv.slice(2));
+var inputfile = process.argv.slice(2);
+fs = require('fs')
+fs.readFile(inputfile, 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data);
+});
 
